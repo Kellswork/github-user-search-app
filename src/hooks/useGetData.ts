236b8effe78fec50b6aedb7bc06291dded38167d
Kellswork <kellogbonna@gmail.com>
2024-10-03 16:50:52 +0100

    hook for suspense

---
+++ src/hooks/useGetData.ts
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import wrapPromise from "../api/wrapPromise";
+
+const useGetData = (url: string, fetcher: (url: string) => any) => {
+  const [resource, setResource] = useState<any>(null);
+
+  useEffect(() => {
+    const _resource = wrapPromise(fetcher(url));
+    setResource(_resource);
+  }, []);
+
+  return resource?.read();
+};
+export default useGetData;
