4138f09df76878682a8887972ddfc3406a7c51e6
Kellswork <kellogbonna@gmail.com>
2024-10-03 03:19:35 +0100

    some updates

--- src/layout.tsx
+++ src/layout.tsx
@@ -2,20 +2,21 @@ import React, { useState } from "react";
 import ThemeToggle from "./components/ThemeToggle";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState("light");
 
-  function toggleTheme(){
-    setTheme(prevTheme => prevTheme === 'light' ? 'dark': 'light');
+  function toggleTheme() {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   }
 
   return (
     <div className="main-container" data-theme={theme}>
-      <header className="main-container__header">
-        <div className="logo">devfinder</div>
-        <ThemeToggle theme={theme} toggleTheme={toggleTheme}/>
-      </header>
-      <main>{children}</main>
-
+      <div className="container-width">
+        <header className="main-container__header">
+          <div className="logo">devfinder</div>
+          <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
+        </header>
+        <main>{children}</main>
+      </div>
     </div>
   );
 }
