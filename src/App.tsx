4ba6d33c28bbab7f5d1a6278b584792e2aa82f2b
Kellswork <kellogbonna@gmail.com>
2024-10-03 22:44:31 +0100

    add usedr bio component

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,19 @@
 import "./styles/main.scss";
 import Layout from "./layout";
-import locationIcon from "./assets/images/icon-location.svg";
-import websiteIcon from "./assets/images/icon-website.svg";
-import twitterIcon from "./assets/images/icon-twitter.svg";
-import comapnyIcon from "./assets/images/icon-company.svg";
 import SearchBar from "./components/SearchBar";
-import userImage from "./assets/images/octocat.svg";
-import UserStats from "./components/UserStats";
-import IconLInk from "./components/IconLink";
+import UserBio from "./components/UserBio";
+import { useState } from "react";
 
 function App() {
+const [selectedUser, setSelectedUser] = useState('octocat')
+
   return (
     <Layout>
-      <SearchBar />
-      <div className="user-bio">
-        <div className="user-bio__header">
-          <div className="user-bio__img">
-            <img src={userImage} alt="" />
-          </div>
-          <div className="user-bio__text">
-            <h1>The Octocat</h1>
-            <h3>@octocat</h3>
-            <p>Joined 25 Jan 2011</p>
-          </div>
-        </div>
-
-        <div className="user-bio__about">
-          <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
-            odio. Quisque volutpat mattis eros.
-          </p>
-        </div>
-        <div className="user-stats">
-          <UserStats label="repos" value={8} />
-          <UserStats label="Followers" value={3938} />
-          <UserStats label="Following" value={9} />
-        </div>
-        <ul className="icon-link__list">
-          <IconLInk
-            name="San Francisco"
-            imgSrc={locationIcon}
-            altText="Location Icon"
-            link=''
-          />
-          <IconLInk
-            name="https://github.blog"
-            imgSrc={websiteIcon}
-            altText="Website Icon"
-            link=''
-          />
-          <IconLInk
-            name="Not Available"
-            imgSrc={twitterIcon}
-            altText="Twitter Icon"
-            link=''
-          />
-          <IconLInk
-            name="@github"
-            imgSrc={comapnyIcon}
-            altText="Company Icon"
-            link=''
-          />
-        </ul>
-      </div>
+      <SearchBar setSelectedUser={setSelectedUser} />
+     
+      <UserBio selecetedUser={selectedUser} />
+    
+  
     </Layout>
   );
 }
---
+++ src/components/UserBio/index.tsx
@@ -0,0 +1,75 @@
+import locationIcon from "../../assets/images/icon-location.svg";
+import websiteIcon from "../../assets/images/icon-website.svg";
+import twitterIcon from "../../assets/images/icon-twitter.svg";
+import comapnyIcon from "../../assets/images/icon-company.svg";
+import UserStats from "../UserStats";
+import IconLInk from "../IconLink";
+import useFetch from "../../hooks/useFetch";
+import { GitHubUserProp } from "./type";
+import { company, formattedDate } from "../../utils/helpers";
+import './index.scss';
+
+export default function UserBio({ selecetedUser }: { selecetedUser: string }) {
+  console.log(selecetedUser);
+  const url = `https://api.github.com/users/${selecetedUser}`;
+  const { data, isLoading, error } = useFetch<GitHubUserProp>(url);
+
+  console.log(isLoading);
+  console.log(error);
+  console.log(data);
+
+
+
+  return (
+    <div className="user-bio">
+      <div className="user-bio__header">
+        <div className="user-bio__img">
+          <img src={data?.avatar_url} alt="" />
+        </div>
+        <div className="user-bio__text">
+          <h1>{data?.name ?? data?.login}</h1>
+          <h3>@{data?.login}</h3>
+          <p>Joined {formattedDate(data?.created_at)}</p>
+        </div>
+      </div>
+
+      <div className="user-bio__about">
+        {data?.bio ? (
+          <p>{data?.bio}</p>
+        ) : (
+          <p className="disabled">This profile has no bio</p>
+        )}
+      </div>
+      <div className="user-stats">
+        <UserStats label="repos" value={data?.public_repos} />
+        <UserStats label="Followers" value={data?.followers} />
+        <UserStats label="Following" value={data?.following} />
+      </div>
+      <ul className="icon-link__list">
+        <IconLInk
+          name={data?.location}
+          imgSrc={locationIcon}
+          altText="Location Icon"
+        />
+        <IconLInk
+          name={data?.blog}
+          imgSrc={websiteIcon}
+          altText="Blog link"
+          link={data?.blog}
+        />
+        <IconLInk
+          name={data?.twitter_username}
+          imgSrc={twitterIcon}
+          altText="Twitter Icon"
+          link={`https://twitter.com/${data?.twitter_username}`}
+        />
+        <IconLInk
+          name={data?.company}
+          imgSrc={comapnyIcon}
+          altText="Company Icon"
+          link={`https://github.com/${company(data?.company)}`}
+        />
+      </ul>
+    </div>
+  );
+}
