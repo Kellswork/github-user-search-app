207cb30353a437482f0ee04f5441d30cf594b426
Kellswork <kellogbonna@gmail.com>
2024-10-04 23:51:53 +0100

    add debounce

---
+++ src/hooks/useDebounce.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+function useDebounce(text: string, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(text);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(text);
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [text, delay]);
+
+  return debouncedValue;
+}
+
+export default useDebounce;
